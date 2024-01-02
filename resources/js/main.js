34e655801678e390985c9459767b0bb69c09aa3f
margotp37 <52484429+margotp37@users.noreply.github.com>
2024-01-02 10:54:08 +0100

    Update main.js

--- resources/js/main.js
+++ resources/js/main.js
@@ -37,5 +37,5 @@ function initializeClock(id, endtime) {
   var timeinterval = setInterval(updateClock, 1000);
 }
 
-var deadline = 'December 25 2023';
+var deadline = 'December 25 2024';
 initializeClock('clockdiv', deadline);
